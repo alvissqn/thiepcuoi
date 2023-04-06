37cbbae3986ee21af884b0c9877e6ee3672d0137
Hoinguyenit <hoinguyenit@gmail.com>
2023-04-06 15:51:40 +0700

    Update Resouce and public

--- resources/views/layouts/editor.blade.php
+++ resources/views/layouts/editor.blade.php
@@ -1,56 +1,56 @@
 @php
-    use \App\Services\NotificationServices;
-    $splitPath = explode('/', Request::path() );
-    $pageParams = [
-        'name' => $splitPath[1] ?? null,
-        'sname' => $splitPath[2] ?? null,
-    ];
-    if( empty($pageParams['sname']) ){
-        $config = config('admin_menu_link')[ $pageParams['name'] ] ?? [];
-    }else{
-        $config = config('admin_menu_link')[ $pageParams['name'] ]['subs'][ $pageParams['sname'] ] ?? [];
-    }
-    $config['load_js_language'] = array_merge(
-        $config['load_js_language'],
-        ['general']
-    );
-    Permission::required($config['permission'], '/');
+use \App\Services\NotificationServices;
+$splitPath = explode('/', Request::path() );
+$pageParams = [
+'name' => $splitPath[1] ?? null,
+'sname' => $splitPath[2] ?? null,
+];
+if( empty($pageParams['sname']) ){
+$config = config('admin_menu_link')[ $pageParams['name'] ] ?? [];
+}else{
+$config = config('admin_menu_link')[ $pageParams['name'] ]['subs'][ $pageParams['sname'] ] ?? [];
+}
+$config['load_js_language'] = array_merge(
+$config['load_js_language'],
+['general']
+);
+Permission::required($config['permission'], '/');
 
 @endphp
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-<head>
-    @section('header')
+    <head>
+        @section('header')
         <title>@yield('title', $config['label'] )</title>
-    <meta charset="UTF-8"/>
-    <meta
-        name="viewport"
-        content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-    <link href="/assets/css/select2.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/spectrum.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/toastr.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/style.css" rel="stylesheet" type="text/css"/>
-    <!--    <link rel="stylesheet" href="css/ruler.css" type="text/css" />-->
-    <link id="palleon-theme-link" href="/assets/css/dark.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/css/custom.css" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" href="/assets/node_modules/ruler.js/dist/ruler.min.css">
-    <script src="/assets/node_modules/ruler.js/dist/ruler.min.js"></script>
-    <!-- <link id="palleon-theme-link" href="css/light.css" rel="stylesheet" type="text/css"> -->
-    @show
-</head>
-<body id="palleon" class="backend">
-    @yield('content')
-    @section('footer')
+        <meta charset="UTF-8"/>
+        <meta
+                name="viewport"
+                content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+        <link href="/assets/photo-editor/css/select2.css" rel="stylesheet" type="text/css"/>
+        <link href="/assets/photo-editor/css/spectrum.css" rel="stylesheet" type="text/css"/>
+        <link href="/assets/photo-editor/css/toastr.css" rel="stylesheet" type="text/css"/>
+        <link href="/assets/photo-editor/css/style.css" rel="stylesheet" type="text/css"/>
+        <!--    <link rel="stylesheet" href="css/ruler.css" type="text/css" />-->
+        <link id="palleon-theme-link" href="/assets/photo-editor/css/dark.css" rel="stylesheet" type="text/css"/>
+        <link href="/assets/photo-editor/css/custom.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="/assets/photo-editor/node_modules/ruler.js/dist/ruler.min.css">
+        <script src="/assets/photo-editor/node_modules/ruler.js/dist/ruler.min.js"></script>
+        <!-- <link id="palleon-theme-link" href="css/light.css" rel="stylesheet" type="text/css"> -->
+        @show
+    </head>
+    <body id="palleon" class="backend">
+        @yield('content')
+        @section('footer')
         <!-- Scripts -->
-        <script src="/assets/js/jquery.min.js"></script>
-        <script src="/assets/js/jquery-ui.min.js"></script>
-        <script src="/assets/js/fabric.min.js"></script>
-        <script src="/assets/js/addons/centering_guidelines.js"></script>
-        <script src="/assets/js/addons/aligning_guidelines.js"></script>
-        <script src="/assets/js/plugins.min.js"></script>
-        <script src="/assets/js/palleon.js"></script>
-        <script src="/assets/js/custom.js"></script>
+        <script src="/assets/photo-editor/js/jquery.min.js"></script>
+        <script src="/assets/photo-editor/js/jquery-ui.min.js"></script>
+        <script src="/assets/photo-editor/js/fabric.min.js"></script>
+        <script src="/assets/photo-editor/js/addons/centering_guidelines.js"></script>
+        <script src="/assets/photo-editor/js/addons/aligning_guidelines.js"></script>
+        <script src="/assets/photo-editor/js/plugins.min.js"></script>
+        <script src="/assets/photo-editor/js/palleon.js"></script>
+        <script src="/assets/photo-editor/js/custom.js"></script>
         <!-- Translation Strings -->
         <script>
             /* <![CDATA[ */
@@ -98,5 +98,5 @@
         </script>
         <!-- Scripts END -->
         @show
-</body>
+    </body>
 </html>
